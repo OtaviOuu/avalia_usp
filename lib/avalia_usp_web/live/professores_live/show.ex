02c9b2546192dfe12b39cb06f1c4bf677792a94a
otaviouu <otavio.venancio.oliveira@usp.br>
2026-03-23 10:02:04 -0300

    init avaliacoes list

--- lib/avalia_usp_web/live/professores_live/show.ex
+++ lib/avalia_usp_web/live/professores_live/show.ex
@@ -14,7 +14,7 @@ defmodule AvaliaUspWeb.ProfessoresLive.Show do
        %{
          professor:
            AvaliaUsp.Professores.get_professor_by_nome_completo!(professor_nome,
-             load: [:disciplinas]
+             load: [:disciplinas, :avaliacoes]
            )
        }}
     end)
@@ -26,21 +26,12 @@ defmodule AvaliaUspWeb.ProfessoresLive.Show do
       <.header>
         Detalhes do Professor
         <:subtitle>sla</:subtitle>
-        <:actions :if={@professor.ok?}>
-          <fieldset class="fieldset">
-            <select class="select">
-              <option disabled selected>Disciplinas</option>
-              <option :for={disciplina <- @professor.result.disciplinas}>
-                {disciplina.nome_completo}
-              </option>
-            </select>
-          </fieldset>
-        </:actions>
       </.header>
       <.async_result :let={professor} assign={@professor}>
         <:loading><.loading_spinner /></:loading>
         <:failed :let={_failure}>erro ao buscar profesosre</:failed>
         <.professor_details professor={professor} />
+        <.avaliacoes_list avaliacoes={professor.avaliacoes} />
       </.async_result>
     </Layouts.app>
     """
@@ -51,6 +42,41 @@ defmodule AvaliaUspWeb.ProfessoresLive.Show do
   defp professor_details(assigns) do
     ~H"""
     {@professor.nome_completo}
+    """
+  end
+
+  attr :avaliacoes, :list, doc: "Lista de avaliações do professor"
+
+  defp avaliacoes_list(assigns) do
+    ~H"""
+    <ul class="list bg-base-100 rounded-box shadow-md">
+      <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Most played songs this week</li>
+
+      <li
+        :for={avaliacao <- @avaliacoes}
+        class="list-row"
+      >
+        <div>
+          <img
+            class="size-10 rounded-box"
+            src="https://img.daisyui.com/images/profile/demo/1@94.webp"
+          />
+        </div>
+        <div>
+          <div>Dio Lupa</div>
+          <div class="text-xs uppercase font-semibold opacity-60">Remaining Reason</div>
+        </div>
+        <p class="list-col-wrap text-xs">
+          {avaliacao.comentario}
+        </p>
+        <.button class="btn btn-square btn-ghost">
+          <.icon name="hero-chevron-up" />
+        </.button>
+        <.button class="btn btn-square btn-ghost">
+          <.icon name="hero-chevron-down" />
+        </.button>
+      </li>
+    </ul>
     """
   end
 end
